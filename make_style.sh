f101af4e018a6442e3048614699ca409217281e2
Ngô Đức Anh <75556609+ducanhgh@users.noreply.github.com>
2023-09-12 23:45:19 +0700

    Refactor `websocket/` from CommonJS to ESM (#324)
    Co-authored-by: Le Bao Hiep <baohiep2013@gmail.com>
    
    (cherry picked from commit 8795a9bcf9ef40296effe1fd5d1b60a055fcefd0)
    
    Proper package checks in `make_style.sh` (#335)
    
    (cherry picked from commit 019edcff6abbb0b384c87f900cb709be020ef15e)

--- make_style.sh
+++ make_style.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-if ! [ -x "$(command -v sass)" ]; then
-  echo 'Error: sass is not installed.' >&2
-  exit 1
-fi
-
-if ! [ -x "$(command -v postcss)" ]; then
-  echo 'Error: postcss is not installed.' >&2
-  exit 1
-fi
-
-if ! [ -x "$(command -v autoprefixer)" ]; then
-  echo 'Error: autoprefixer is not installed.' >&2
-  exit 1
-fi
-
 cd "$(dirname "$0")" || exit
+
+node scripts/check-package-installed.js postcss sass autoprefixer || exit
 
 build_style() {
   echo "Creating $1 style..."
   cp resources/vars-$1.scss resources/vars.scss
-  sass resources:sass_processed
-  postcss sass_processed/style.css sass_processed/martor-description.css sass_processed/select2-dmoj.css --verbose --use autoprefixer -d $2
+  npx sass resources:sass_processed
+  npx postcss sass_processed/style.css sass_processed/martor-description.css sass_processed/select2-dmoj.css --verbose --use autoprefixer -d $2
 }
 
 build_style 'default' 'resources'
